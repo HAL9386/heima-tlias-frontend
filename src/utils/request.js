be864846d6e47dfa4e8f0e523422097ca8f363aa
smile <mad024039@gmail.com>
2025-05-04 15:36:14 +0800

    refactor: 配置代理服务器

--- src/utils/request.js
+++ src/utils/request.js
@@ -1,12 +1,8 @@
 import axios from "axios"
 
 const request = axios.create({
-  baseURL: 'https://m1.apifoxmock.com/m1/6270372-5964539-default',
+  baseURL: '/api',
   timeout: 10 * 60 * 1000, // 10 minutes
-  params: {
-    apifoxApiId: '291715989',
-    apifoxToken: 'OO_sn0SVdz6Y48zdxRJw-'
-  }
 })
 
 request.interceptors.response.use(
--- src/views/dept/index.vue
+++ src/views/dept/index.vue
@@ -35,8 +35,8 @@ onMounted(() => {
     <el-table :data="deptList" border style="width: 100%">
       <el-table-column type="index" label="序号" align="center" width="100"/>
       <el-table-column prop="name" label="部门名称" align="center"/>
-      <el-table-column prop="address" label="最后操作时间" align="center"/>
-      <el-table-column prop="address" label="操作" align="center">
+      <el-table-column prop="updateTime" label="最后操作时间" align="center"/>
+      <el-table-column fixed="right" label="操作" align="center">
         <template #default="scope">
           <el-button type="primary" size="small"><el-icon><Edit /></el-icon> 编辑</el-button>
           <el-button type="danger" size="small"><el-icon><Delete /></el-icon> 删除</el-button>
